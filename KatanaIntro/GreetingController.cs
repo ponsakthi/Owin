d3c57dc466928311961ee80096254aff8384c6c7
ponsakthi <ponsakthisabarikumar@gmail.com>
2014-08-12 15:37:14 +0530

    IIS setup

--- KatanaIntro/GreetingController.cs
+++ KatanaIntro/GreetingController.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace KatanaIntro
 {
     public class GreetingController : ApiController
     {
-
         public Greeting Get()
         {
             return new Greeting() {Text = "hello from Greeting controller"};
--- KatanaIntro/Program.cs
+++ KatanaIntro/Program.cs
@@ -12,19 +12,19 @@ namespace KatanaIntro
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Press a key to get started");
-            Console.ReadKey();
-            var uri = "http://localhost:8080";
-            using (WebApp.Start<Startup>(uri))
-            {
-                Console.WriteLine("Started");
-                Console.ReadKey();
-            }
-            Console.WriteLine("Stopped");
-            Console.ReadLine();
-        }
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Press a key to get started");
+        //    Console.ReadKey();
+        //    var uri = "http://localhost:8080";
+        //    using (WebApp.Start<Startup>(uri))
+        //    {
+        //        Console.WriteLine("Started");
+        //        Console.ReadKey();
+        //    }
+        //    Console.WriteLine("Stopped");
+        //    Console.ReadLine();
+        //}
     }
 
     public class Startup
